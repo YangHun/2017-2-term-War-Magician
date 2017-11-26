f0cd49a1f97eac8d02f5ac59ec64f56a14af3ffb
YangHun <pitahano@gmail.com>
2017-11-26 22:47:24 +0900

    Add MagicCircleInputManager, modify LeapMotion shader
    Former-commit-id: a0fdf78494992de5224c36f85b309ed419cc1fa3

---
+++ War-Magician/Assets/Scripts/Debug/InnerCircle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerCircle : MonoBehaviour {
+
+    public float angle;
+    public float fadeSpeed;
+
+	// Use this for initialization
+	void Start () {
+        Color c = Color.white;
+        c.a = 0.0f;
+        GetComponent<SpriteRenderer>().material.color = c;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0.0f, 0.0f, angle) * Time.deltaTime);
+	}
+
+  
+}
---
+++ War-Magician/Assets/Scripts/Debug/LTD.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LTD : MonoBehaviour {
+
+    public Transform rightIndexBone3;
+    private LineRenderer _renderer;
+    public float distance;
+	// Use this for initialization
+	void Start () {
+        _renderer = GetComponent<LineRenderer>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = rightIndexBone3.forward;
+
+        _renderer.SetPosition(0, rightIndexBone3.position);
+        _renderer.SetPosition(1, pos * distance + rightIndexBone3.position);
+
+	}
+}
