9f479eb26e60d8db154923d0dfa8b0c0e2712741
YangHun <pitahano@gmail.com>
2017-12-03 16:32:35 +0900

    recreate continuous graph managing
    Former-commit-id: 45415f49d46fc1531e02866e9021d16e83736bf7

---
+++ War-Magician/Assets/Scripts/ImageProcessor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TensorFlow;
+using System.Text;
+
+public class ImageProcessor : MonoBehaviour {
+
+    public TextAsset frozenGraph;
+
+    TFGraph graph;
+    TFSession session;
+    
+	// Use this for initialization
+	void Start () {
+        graph = new TFGraph();
+        
+        if(frozenGraph != null)
+        {
+            graph.Import(frozenGraph.bytes);
+        }
+
+        session = new TFSession(graph);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator StartPredict(byte[] bytes)
+    {
+        TFSession.Runner runner = session.GetRunner();
+
+        
+        runner.AddInput(graph["binary"][0], TFTensor.CreateString(bytes) );
+        runner.Fetch(graph["InceptionV3/Predictions/Reshape_1"][0]);
+        
+        float[,] result = runner.Run()[0].GetValue() as float[,];
+        float[] output = new float[result.GetLength(1)];
+
+        for (int i = 0; i < result.GetLength(1); i++) {
+
+            output[i] = result[0, i];
+            Debug.Log(i + " :" + result[0, i]);
+        }
+
+        MagicCircleInputManager.I.predictions = output;
+
+        yield return null;
+
+    }
+
+
+}
