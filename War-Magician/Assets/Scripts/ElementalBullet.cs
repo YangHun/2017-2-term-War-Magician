1ecf563a24d970634e4c156dd317116a2b189101
YangHun <pitahano@gmail.com>
2017-12-11 01:58:25 +0900

    AI Turret Complete
    Former-commit-id: e73f7d100f556e5454a8398693930d8b4f822da2

--- War-Magician/Assets/Scripts/ElementalBullet.cs
+++ War-Magician/Assets/Scripts/ElementalBullet.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public class ElementalBullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    ParticleSystem _system;
+    bool isCollided = false;
+    float timer = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        _system = GetComponent<ParticleSystem>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+      if (isCollided)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 10.0f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
 	}
 
     void OnParticleCollision(GameObject obj)
     {
-        if (obj.tag == "FieldMonster")
+        if (obj.tag == "FieldMonster" || obj.tag == "AirMonster" )
         {
             obj.GetComponent<Monster_HP>().GetDamaged(20);
         }
+        
 
         Debug.Log(obj.tag);
-        
+
+        if (!isCollided)
+        {
+            isCollided = true;
+        }
+
     }
 
 
