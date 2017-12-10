cc5d67e5588292144a3793cd90fb3ad8540e3719
DESKTOP-LM7J5QI\Undertale <mk970205@gmail.com>
2017-12-10 13:39:52 +0900

    Monster animation, and All monster
    Former-commit-id: f097c8ff98eaf3e89a6a4d890fe1eaeb16222f28

--- War-Magician/Assets/Scripts/AI_AIR.cs
+++ War-Magician/Assets/Scripts/AI_AIR.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AI_AIR : MonoBehaviour {
     public GameObject target;
+    public float speed;
+    bool isInitialized = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,27 @@ public class AI_AIR : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if(!isInitialized && target != null)
+        {
+            if (GetComponent<Animator>() != null)
+            {
+                GetComponent<Animator>().SetTrigger("Come Closer");
+            }
+            isInitialized = true;
+        }
+        /*
+        if (Vector3.Distance(target.transform.position, transform.position) < 2f)
+        {
+            return;
+        }
+        else
+        {
+            GetComponent<CharacterController>().Move((target.transform.position - transform.position) * speed * Time.deltaTime);
+
+        }*/
+    }
+    public void Init()
+    {
+
+    }
 }
--- War-Magician/Assets/Scripts/MonsterPool.cs
+++ War-Magician/Assets/Scripts/MonsterPool.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 using UnityEngine.AI;
 public class MonsterPool : MonoBehaviour {
     public GameObject MonsterNormal;
-   // public GameObject MonsterFlying;
+    public GameObject MonsterFly;
+    public GameObject MonsterBird;
     public GameObject MonsterTotem;
     //  public GameObject MonsterBoss;
     public GameObject MonsterSwarm;
-    public GameObject target;
-
-    int initialPoolSize = 200;
-    int addonPoolSize = 40;
+    public GameObject MonsterShield;
+    public GameObject target_field;
+    public GameObject[] target_air;
+    int initialPoolSize = 20;
+    int addonPoolSize = 20;
 
     public enum Category
     {
         NORMAL = 0,
-        TOTEM, //,, FLYING, BOSS
-        SWARM
+        TOTEM,
+        SWARM,
+        SHIELD,
+        FLY,
+        BIRD
     };
 
     Dictionary<Spawnpoint, Dictionary<GameObject, List<GameObject>>> ObjectPool;
@@ -26,40 +31,83 @@ public class MonsterPool : MonoBehaviour {
     {
         int categoryLength = System.Enum.GetValues(typeof(Category)).Length;
         ObjectPool = new Dictionary<Spawnpoint, Dictionary<GameObject, List<GameObject>>>();
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < GetComponent<MonsterSpawner>().point_field.Length; i++)
         {
             Dictionary<GameObject, List<GameObject>> ObjectPoolPool = new Dictionary<GameObject, List<GameObject>>();
             for(int j = 0; j < categoryLength; j++)
             {
                 GameObject tempCategory = ClassifyEnumObject((Category)j);
-                List<GameObject> ObjectPoolPoolPool = new List<GameObject>();
-                for(int k = 0; k < initialPoolSize; k++)
+                if(tempCategory == MonsterNormal || tempCategory == MonsterShield || tempCategory == MonsterTotem)
                 {
-                    GameObject g = Instantiate(tempCategory);
-
-                    if (g.GetComponent<AI_FIELD>() != null)
+                    List<GameObject> ObjectPoolPoolPool = new List<GameObject>();
+                    RaycastHit hit;
+                    Physics.Raycast(GetComponent<MonsterSpawner>().Spawnpoint_field[i].Pivot.position, Vector3.down, out hit);
+                    for (int k = 0; k < initialPoolSize; k++)
                     {
-                        RaycastHit hit;
-                        Physics.Raycast(GetComponent<MonsterSpawner>().Spawnpoint[i].Pivot.position, Vector3.down, out hit);
+                        GameObject g = Instantiate(tempCategory);
                         g.transform.position = hit.point + new Vector3(0, g.GetComponent<CapsuleCollider>().height, 0);
                         g.transform.rotation = Quaternion.identity;
-                        g.GetComponent<AI_FIELD>().target = target;
+                        g.GetComponent<AI_FIELD>().target = target_field;
+                        g.SetActive(false);
+                        ObjectPoolPoolPool.Add(g);
                     }
-                    else if(g.GetComponent<AI_AIR>() != null)
-                    {
-                        g.GetComponent<AI_AIR>().target = target;
-                    }
-                    g.SetActive(false);
-                    ObjectPoolPoolPool.Add(g);
+                    ObjectPoolPool.Add(tempCategory, ObjectPoolPoolPool);
                 }
-                ObjectPoolPool.Add(tempCategory, ObjectPoolPoolPool);
             }
-            ObjectPool.Add(GetComponent<MonsterSpawner>().Spawnpoint[i], ObjectPoolPool);
+            ObjectPool.Add(GetComponent<MonsterSpawner>().Spawnpoint_field[i], ObjectPoolPool);
+        }
+        for(int i = 0; i < GetComponent<MonsterSpawner>().point_air.Length; i++)
+        {
+            Dictionary<GameObject, List<GameObject>> ObjectPoolPool = new Dictionary<GameObject, List<GameObject>>();
+            for (int j = 0; j < categoryLength; j++)
+            {
+                GameObject tempCategory = ClassifyEnumObject((Category)j);
+                if (tempCategory == MonsterFly || tempCategory == MonsterBird) {
+                    List<GameObject> ObjectPoolPoolPool = new List<GameObject>();
+                    for (int k = 0; k < initialPoolSize; k++)
+                    {
+                        GameObject g = Instantiate(tempCategory);
+                        g.transform.position = GetComponent<MonsterSpawner>().Spawnpoint_air[i].Pivot.position;
+                        g.transform.rotation = Quaternion.identity;
+                        g.GetComponent<AI_AIR>().target = target_air[i];
+                        g.SetActive(false);
+                        ObjectPoolPoolPool.Add(g);
+                    }
+                    ObjectPoolPool.Add(tempCategory, ObjectPoolPoolPool);
+                }
+            }
+            ObjectPool.Add(GetComponent<MonsterSpawner>().Spawnpoint_air[i], ObjectPoolPool);
+        }
+        for (int i = 0; i < GetComponent<MonsterSpawner>().point_swarm.Length; i++)
+        {
+            Dictionary<GameObject, List<GameObject>> ObjectPoolPool = new Dictionary<GameObject, List<GameObject>>();
+            for (int j = 0; j < categoryLength; j++)
+            {
+                GameObject tempCategory = ClassifyEnumObject((Category)j);
+                if (tempCategory == MonsterSwarm)
+                {
+                    List<GameObject> ObjectPoolPoolPool = new List<GameObject>();
+                    RaycastHit hit;
+                    Physics.Raycast(GetComponent<MonsterSpawner>().Spawnpoint_swarm[i].Pivot.position, Vector3.down, out hit);
+                    for (int k = 0; k < initialPoolSize; k++)
+                    {
+                        GameObject g = Instantiate(tempCategory);
+                        g.transform.position = hit.point + new Vector3(0, g.GetComponent<CapsuleCollider>().height, 0);
+                        g.transform.rotation = Quaternion.identity;
+                        g.GetComponent<AI_FIELD>().target = target_field;
+                        g.SetActive(false);
+                        ObjectPoolPoolPool.Add(g);
+                    }
+                    ObjectPoolPool.Add(tempCategory, ObjectPoolPoolPool);
+                }
+            }
+            ObjectPool.Add(GetComponent<MonsterSpawner>().Spawnpoint_swarm[i], ObjectPoolPool);
         }
     }
 
     public GameObject GetObject(Spawnpoint point, Category input)
     {
+        GetComponent<MonsterSpawner>().NumOfMonster++;
         List<GameObject> list = ObjectPool[point][ClassifyEnumObject(input)];
         for (int i = 0; i < list.Count; i++)
         {
@@ -88,16 +136,21 @@ public class MonsterPool : MonoBehaviour {
                 return MonsterNormal;
             case Category.TOTEM:
                 return MonsterTotem;
+            
+            case Category.SHIELD:
+                return MonsterShield;
+                
+            case Category.FLY:
+                return MonsterFly;
             case Category.SWARM:
                 return MonsterSwarm;
                 /*
-            case Category.FLYING:
-                return MonsterFlying;
-            
             case Category.BOSS:
                 return MonsterBoss;
                 */
 
+            case Category.BIRD:
+                return MonsterBird;
         }
         return null;
     }
