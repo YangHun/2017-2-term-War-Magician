26d507d9302359b5104431ed7cb741090776a3a0
YangHun <pitahano@gmail.com>
2017-12-10 17:01:48 +0900

    fix scripts to be able to build
    Former-commit-id: 2727b7a83c4b73176d8212d47049cf53fda0ac62

--- War-Magician/Assets/Scripts/MonsterManager.cs
+++ War-Magician/Assets/Scripts/MonsterManager.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterManager : MonoBehaviour {
-<<<<<<< Updated upstream
+
     MonsterSpawner MS;
 
     void Start()
     {
         MS = GetComponent<MonsterSpawner>();
     }
-=======
-    
->>>>>>> Stashed changes
+
 }
